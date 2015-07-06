1aa03ca1063d3b96f00996dd54235d9841748a1f
Pavel Nadolski <tbigad@gmail.com>
2015-07-06 17:17:01 +0300

    it's not a bug, it's a feature

--- notABag/mainwindow.cpp
+++ notABag/mainwindow.cpp
@@ -6,7 +6,7 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    test = std::make_shared<TestWidget>(new TestWidget(this));
+    test = std::make_shared<TestWidget>(this);
 }
 
 MainWindow::~MainWindow()
