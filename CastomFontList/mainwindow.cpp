a603b4d233df7ac5073c9e68c59d90ccf4f73437
Pavel Nadolski <tbigad@gmail.com>
2015-08-22 17:39:41 +0300

    clean repo

--- CastomFontList/mainwindow.cpp
+++ CastomFontList/mainwindow.cpp
@@ -2,6 +2,8 @@
 #include "ui_mainwindow.h"
 #include <QFontDatabase>
 #include <QComboBox>
+#include <QDebug>
+#include <QAction>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -10,14 +12,16 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->setupUi(this);
 
     QFontDatabase database;
-    QStringList items = database.families();
+    QStringList fonts;
+    fonts <<"Arial"<<"Comic Sans MS"<<"Courier New"<<"Georgia"<<"Impact"<<"Times New Roman"
+                            <<"Trebuchet MS"<<"Verdana";
     ui->comboBox->setStyleSheet("border-radius: 4px;"
                               "background-color: #3b3b3b;"
                               "selection-background-color: #808080;"
                               "color: #ffffff;"
                               "font-size: 12pt;"
-                              "down-arrow: {border:none;}");
-    ui->comboBox->addItems(items);
+                              "QComboBox::drop-down {border-width: 0px;} QComboBox::down-arrow {image: url(noimg); border-width: 0px;}");
+    ui->comboBox->addItems(fonts);
     ui->comboBox->setMinimumWidth(200);
     ui->label->setText(ui->comboBox->currentText());
     ui->label->setFont(QFont(ui->comboBox->currentText(),12,10));
@@ -25,6 +29,28 @@ MainWindow::MainWindow(QWidget *parent) :
     connect(ui->comboBox,(void (QComboBox:: *)(int)) &QComboBox::highlighted,[=](int index){
         ui->label->setText(ui->comboBox->itemText(index));
         ui->label->setFont(QFont(ui->comboBox->itemText(index),12,0));
+    });
+//-----------------------------------------------------------------------------------------------------------
+    QMenu *menu = new QMenu();
+    ui->pushButton->setMenu(menu);
+    menu->setStyleSheet("border-radius: 4px;"
+                        "background-color: #3b3b3b;"
+                        "selection-background-color: #808080;"
+                        "color: #ffffff;"
+                        "font-size: 12pt;");
+    QStringList list;
+    list <<"Arial"<<"Comic Sans MS"<<"Courier New"<<"Georgia"<<"Impact"<<"Times New Roman"
+            <<"Trebuchet MS"<<"Verdana";
+    foreach (QString fontFamily, list) {
+        if(database.hasFamily(fontFamily)){
+            QAction *item = new QAction(menu);
+            item->setText(fontFamily);
+            item->setFont(QFont(fontFamily));
+            menu->addAction(item);
+        }
+    }
+    connect(menu, &QMenu::hovered, [=](QAction *item){
+        ui->label->setText(item->text());
     });
 }
 
--- notABag/mainwindow.cpp
+++ notABag/mainwindow.cpp
@@ -6,7 +6,7 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    test = std::make_shared<TestWidget>(this);
+    test = std::make_shared<TestWidget>(new TestWidget);
 }
 
 MainWindow::~MainWindow()
